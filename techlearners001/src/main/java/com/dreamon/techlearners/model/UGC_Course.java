60e79cf23f43598c1bf9a29ca93e1e87810812bd
Derush <k.derushan@gmail.com>
2017-08-30 17:31:08 +0530

    change

--- techlearners001/src/main/java/com/dreamon/techlearners/model/UGC_Course.java
+++ techlearners001/src/main/java/com/dreamon/techlearners/model/UGC_Course.java
@@ -6,12 +6,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.HashMap;
 import java.util.Map;
 
-@Document(collection = "UGC")
+@Document(collection = "SuPathWay.PhisicalScience")
 public class UGC_Course {
 
     @Id
     private String id;
-    private Map<String, Integer> eligibility = new HashMap();
+    private String name;
+
+    private Map<String, Double> eligibility = new HashMap();
 
     public String getId() {
         return id;
@@ -21,11 +23,19 @@ public class UGC_Course {
         this.id = id;
     }
 
-    public Map<String, Integer> getEligibility() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Double> getEligibility() {
         return eligibility;
     }
 
-    public void setEligibility(Map<String, Integer> eligibility) {
+    public void setEligibility(Map<String, Double> eligibility) {
         this.eligibility = eligibility;
     }
 }
