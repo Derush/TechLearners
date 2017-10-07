666397594d713f5f18c7a8c144e8dd1f93737a40
parkavi11 <parkavi113@gmail.com>
2017-10-07 15:50:03 +0530

    my

--- techlearners001/src/main/java/com/dreamon/techlearners/controller/aboutController.java
+++ techlearners001/src/main/java/com/dreamon/techlearners/controller/aboutController.java
@@ -1,12 +1,14 @@
 package com.dreamon.techlearners.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class aboutController {
 
     @RequestMapping("/about")
-    public String pageform(Model model) {
+    public String about1(Model model) {
 //        ugcRepository.findAll();
         return "about";
     }
