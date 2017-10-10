c33259a570b3beaed427def51930fb68fa89f721
thamee <thameera808@gmail.com>
2017-10-10 21:42:42 +0530

    commi

---
+++ techlearners001/src/main/java/com/dreamon/techlearners/model/favCourse.java
@@ -0,0 +1,68 @@
+package com.dreamon.techlearners.model;
+
+public class favCourse {
+    private String id;
+    private String subject1;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSubject1() {
+        return subject1;
+    }
+
+    public void setSubject1(String subject1) {
+        this.subject1 = subject1;
+    }
+
+    public String getSubject2() {
+        return subject2;
+    }
+
+    public void setSubject2(String subject2) {
+        this.subject2 = subject2;
+    }
+
+    public String getSubject3() {
+        return subject3;
+    }
+
+    public void setSubject3(String subject3) {
+        this.subject3 = subject3;
+    }
+
+    public Integer getSubject1R() {
+        return subject1R;
+    }
+
+    public void setSubject1R(Integer subject1R) {
+        this.subject1R = subject1R;
+    }
+
+    public Integer getSubject2R() {
+        return subject2R;
+    }
+
+    public void setSubject2R(Integer subject2R) {
+        this.subject2R = subject2R;
+    }
+
+    public Integer getSubject3R() {
+        return subject3R;
+    }
+
+    public void setSubject3R(Integer subject3R) {
+        this.subject3R = subject3R;
+    }
+
+    private String subject2;
+    private String subject3;
+    private Integer subject1R;
+    private Integer subject2R;
+    private Integer subject3R;
+}
