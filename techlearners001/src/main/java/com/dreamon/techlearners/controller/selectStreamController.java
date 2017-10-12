d2af76972ff8bb97cfd0625512ec7d3ad0d9d357
thamee <thameera808@gmail.com>
2017-10-12 06:20:23 +0530

    commi

--- techlearners001/src/main/java/com/dreamon/techlearners/controller/selectStreamController.java
+++ techlearners001/src/main/java/com/dreamon/techlearners/controller/selectStreamController.java
@@ -2,9 +2,11 @@ package com.dreamon.techlearners.controller;
 
 import com.dreamon.techlearners.model.StoreList;
 import com.dreamon.techlearners.model.UGC_Course;
+import com.dreamon.techlearners.model.stream;
 import com.dreamon.techlearners.repository.StoreListRepository;
 import com.dreamon.techlearners.repository.UGCRepository;
 import com.dreamon.techlearners.repository.eligibleRepository;
+import com.dreamon.techlearners.repository.streamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,9 +20,7 @@ import java.util.Map;
 @Controller
 public class selectStreamController {
     @Autowired
-    eligibleRepository eligi;
-    @Autowired
-    UGCRepository course;
+    streamRepository st;
     @Autowired
     StoreListRepository retrivelist;
     @RequestMapping("/selectStream")
@@ -38,52 +38,72 @@ public class selectStreamController {
         String subject51 = request.getParameter("subject51");
         String subject52 = request.getParameter("subject52");
         String subject53 = request.getParameter("subject53");
-        Integer subject1R = Integer.valueOf(request.getParameter("subject1R"));
-        Integer subject2R = Integer.valueOf(request.getParameter("subject2R"));
-        Integer subject3R = Integer.valueOf(request.getParameter("subject3R"));
         StoreList sl = new StoreList();
-        List<UGC_Course> newList = course.findAll();
+        List<stream> newList =st.findAll();
 
         int cou = 0;
         for (int i = 0; i < newList.size(); i++) {
             Map<String, Integer> checking = newList.get(i).getEligibility();
-            String coursename = newList.get(i).getName();
-            String university = newList.get(i).getUniversity();
-
+            String name = newList.get(i).getName();
             boolean check1 = false;
             boolean check2 = false;
             boolean check3 = false;
 
-            for (Map.Entry<String, Integer> entry : checking.entrySet()) {
-                String key = entry.getKey().toString();
-                Integer value = entry.getValue();
-                System.out.println("key, " + key + " value " + value);
-                if ((key.equals(subject51)) && (value <= subject1R)) {
-                    check1 = true;
+            if (name.equals(subject51) || name.equals(subject52) || name.equals(subject53)) {
+                for (Map.Entry<String, Integer> entry : checking.entrySet()) {
+                    String key = entry.getKey().toString();
+                    Integer value = entry.getValue();
+                    System.out.println(" you have to take "+key+" in Ordinary level and should get"+ value +" passes"+" or ");
+
+
                 }
 
-                if ((key.equals(subject52)) && (value <= subject2R)) {
-                    check2 = true;
-                }
+        }
+        else
 
-                if ((key.equals(subject53)) && (value <= subject3R)) {
-                    check3 = true;
-                }
+                System.out.println("you are qualified for this combinations");
+            }
+        model.addAttribute("show", retrivelist.findAll());
+        return "showCourses";
+    }
 
+    @RequestMapping(name = "/arts", value = "/arts", method = RequestMethod.POST)
+    public String check2(HttpServletRequest request, Model model) {
+
+        // retrivelist.deleteAll();
+
+        String subject51 = request.getParameter("subject51");
+        String subject52 = request.getParameter("subject52");
+        String subject53 = request.getParameter("subject53");
+        StoreList sl = new StoreList();
+        List<stream> newList =st.findAll();
+
+        int cou = 0;
+        for (int i = 0; i < newList.size(); i++) {
+            Map<String, Integer> checking = newList.get(i).getEligibility();
+            String name = newList.get(i).getName();
+            boolean check1 = false;
+            boolean check2 = false;
+            boolean check3 = false;
+
+            if (name.equals(subject51) || name.equals(subject52) || name.equals(subject53)) {
+                for (Map.Entry<String, Integer> entry : checking.entrySet()) {
+                    String key = entry.getKey().toString();
+                    Integer value = entry.getValue();
+                    System.out.println(" you have to take "+key+" in Ordinary level and should get"+ value +" passes"+" or ");
+
+
+                }
 
             }
-            if (check1 == true && check2 == true && check3 == true) {
-                sl.setId(String.valueOf(cou));
-                sl.setName(coursename);
-                sl.setUniversity(university);
-                retrivelist.save(sl);
-                cou++;
+            else
 
-            } else
-                System.out.println("no subject there to choose");
+                System.out.println("you are qualified for this combinations");
         }
         model.addAttribute("show", retrivelist.findAll());
         return "showCourses";
     }
+
+
 }
 
