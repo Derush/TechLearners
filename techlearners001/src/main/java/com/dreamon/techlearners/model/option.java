69315d16477712c15f103dce2da9991f47c1358d
thamee <thameera808@gmail.com>
2017-11-03 21:50:42 +0530

    comi

---
+++ techlearners001/src/main/java/com/dreamon/techlearners/model/option.java
@@ -0,0 +1,67 @@
+package com.dreamon.techlearners.model;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.data.annotation.Id;
+
+
+@Document(collection = "Stream")
+public class option {
+
+    @Id
+    private String id;
+    private String name;
+    private String option1;
+    private String option2;
+    private String option3;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public String val;
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+}
